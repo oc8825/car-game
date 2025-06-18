05d89734d1b8268c079c5539752991014022e550
Owen Clarke <oc8825@princeton.edu>
2025-06-18 11:23:33 -0400

    more resizing - creating container for game

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -1,32 +1,42 @@
 import React, { useEffect, useRef } from 'react';
 import { buildPhaserGame } from '/src/components/GameScene';
 
+
 const GameCanvas = () => {
   const gameContainerRef = useRef(null);
 
   useEffect(() => {
+    // ensure game doesn't get covered by UI elements on mobile
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    setViewportHeight();
+    window.addEventListener('resize', setViewportHeight);
+    window.addEventListener('orientationchange', setViewportHeight);
+
     const phaserGame = buildPhaserGame({
       parent: gameContainerRef.current,
     });
 
     return () => {
+      window.removeEventListener('resize', setViewportHeight);
+      window.removeEventListener('orientationchange', setViewportHeight);
+
       phaserGame.destroy(true);
     };
   }, []);
 
      return (
-        <div
+        <div id="game-container">
+          <div
             ref={gameContainerRef}
             style={{ 
-              width: '100vw', 
-              height: '100vh', 
-              overflow: 'hidden', 
-              position: 'absolute', 
-              top: 0, 
-              left: 0,
-              margin: 0,
-              padding: 0}}
-        />
+              width: '100%', 
+              height: '100%', 
+            }}
+          />
+        </div>
     );
 };
 
