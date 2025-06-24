e22648879a8412efb3e2c3827924268f5ea3c1ce
Owen Clarke <oc8825@princeton.edu>
2025-06-24 12:42:51 -0400

    start background

--- src/components/startScreen.jsx
+++ src/components/startScreen.jsx
@@ -23,14 +23,14 @@ export default class startScreen extends Phaser.Scene {
         lockOrientation(this);
 
         // background
-        let background = this.add.image(0, 0, 'tempBackground');
+        let background = this.add.image(0, 0, 'startBackground');
         background.setOrigin(0, 0); 
         background.setDisplaySize(this.scale.width, BASE_GAME_HEIGHT); 
         background.setScale(Math.max(this.scale.width / background.width, BASE_GAME_HEIGHT / background.height));
 
         // play button
-        this.playButton = this.add.image(this.scale.width / 2, BASE_GAME_HEIGHT * 0.75, 'playButton').setInteractive();
-        this.playButton.setScale(0.5);
+        this.playButton = this.add.image(this.scale.width / 2, BASE_GAME_HEIGHT * 0.7, 'playButton').setInteractive();
+        this.playButton.setScale(0.7);
         this.playButton.on('pointerdown', () => {
             this.scene.start('instructionsScreen');
             this.gameStartSound.play();
